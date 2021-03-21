efc200491f69530529a19f4d18bbdd32f0e420c8
Leonards03 <edwin.leonards03@gmail.com>
2021-03-21 23:22:07 +0700

    Update ViewBinding
    set ViewBinding by lazy variable

--- app/src/main/java/com/dicoding/bfaa/githubuser/activity/MainActivity.kt
+++ app/src/main/java/com/dicoding/bfaa/githubuser/activity/MainActivity.kt
@@ -3,10 +3,15 @@ package com.dicoding.bfaa.githubuser.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.bfaa.githubuser.R
+import com.dicoding.bfaa.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
