bc33d471425791c6cfcbe0ef9e505c0499a70609
Leonards03 <edwin.leonards03@gmail.com>
2021-03-21 23:22:29 +0700

    Update User.kt

--- app/src/main/java/com/dicoding/bfaa/githubuser/data/entity/User.kt
+++ app/src/main/java/com/dicoding/bfaa/githubuser/data/entity/User.kt
@@ -1,13 +1,15 @@
 package com.dicoding.bfaa.githubuser.data.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class User(
     val username: String,
     val name: String,
     val avatar: Int,
-    val follower: Int, // arrayList?
-    val following: Int, // arrayList?
     val location: String,
-    val repository: Int // arrayList?
-)
-
-// few items are subject to change because of the possibility i could made
+    val repository: Int, // arrayList?
+    val follower: Int, // arrayList?
+    val following: Int // arrayList?
+) : Parcelable
