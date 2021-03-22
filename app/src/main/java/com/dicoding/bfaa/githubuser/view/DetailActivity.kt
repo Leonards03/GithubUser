8ce0b7352748755a1e0e87b75672eba7955fc015
Leonards03 <edwin.leonards03@gmail.com>
2021-03-23 01:59:54 +0700

    Refactor DetailsActivity.kt

---
+++ app/src/main/java/com/dicoding/bfaa/githubuser/view/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.bfaa.githubuser.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.bfaa.githubuser.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
