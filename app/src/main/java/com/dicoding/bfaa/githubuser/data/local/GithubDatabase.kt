a63c4d73c4bbb34a841ed53063e4c260498afb47
Leonards03 <edwin.leonards03@gmail.com>
2021-04-18 20:07:29 +0700

    Add base uri for ContentProvider

--- app/src/main/java/com/dicoding/bfaa/githubuser/data/local/GithubDatabase.kt
+++ app/src/main/java/com/dicoding/bfaa/githubuser/data/local/GithubDatabase.kt
@@ -1,5 +1,6 @@
 package com.dicoding.bfaa.githubuser.data.local
 
+import android.net.Uri
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.dicoding.bfaa.githubuser.data.local.dao.RepositoryDao
@@ -22,4 +23,20 @@ import com.dicoding.bfaa.githubuser.data.local.entity.UserEntity
 abstract class GithubDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun repoDao(): RepositoryDao
+
+    companion object {
+        const val AUTHORITY = "com.dicoding.bfaa.githubuser"
+        private const val SCHEME = "content"
+        const val USERS_TABLE = "users"
+        const val FOLLOWERS_TABLE = "followers"
+        const val FOLLOWING_TABLE = "following"
+        const val REPOSITORY_TABLE = "repositories"
+
+
+        val CONTENT_URI: Uri = Uri.Builder()
+            .scheme(SCHEME)
+            .authority(AUTHORITY)
+            .appendPath(USERS_TABLE)
+            .build()
+    }
 }
