4803f1adb55bcb60cb8d5614b156f5c981f70ebf
Leonards03 <edwin.leonards03@gmail.com>
2021-04-19 16:02:51 +0700

    Update FavoriteViewModel.kt

--- app/src/main/java/com/dicoding/bfaa/githubuser/view/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dicoding/bfaa/githubuser/view/ui/favorite/FavoriteViewModel.kt
@@ -1,12 +1,17 @@
 package com.dicoding.bfaa.githubuser.view.ui.favorite
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.dicoding.bfaa.githubuser.data.model.User
 import com.dicoding.bfaa.githubuser.data.repository.MainRepository
 import com.dicoding.bfaa.githubuser.di.IoDispatcher
 import com.dicoding.bfaa.githubuser.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,13 +20,27 @@ class FavoriteViewModel @Inject constructor(
     @IoDispatcher
     private val ioDispatcher: CoroutineDispatcher
 ) : ViewModel() {
-    private val _favoriteUser = liveData(ioDispatcher) {
-        emit(Resource.loading(null))
-        try {
-            emit(Resource.success(mainRepository.getFavoriteList()))
-        } catch (exception: Exception) {
-            emit(Resource.error(null, message = exception.message ?: "Error occured"))
+
+    init {
+        viewModelScope.launch {
+            loadList()
         }
     }
-    val favoriteUser get() = _favoriteUser
+
+    private val _favoriteUser: MutableLiveData<Resource<List<User>>> = MutableLiveData()
+    val favoriteUser get(): LiveData<Resource<List<User>>> = _favoriteUser
+
+    suspend fun loadList() = withContext(ioDispatcher) {
+        _favoriteUser.postValue(Resource.loading(null))
+        try {
+            _favoriteUser.postValue(Resource.success(mainRepository.getFavoriteList()))
+        } catch (exception: Exception) {
+            _favoriteUser.postValue(
+                Resource.error(
+                    null,
+                    message = exception.message ?: "Error occured"
+                )
+            )
+        }
+    }
 }
