a98f47db9190b46c3aa38bc5cb88da83107a1bad
Leonards03 <edwin.leonards03@gmail.com>
2021-04-23 09:33:31 +0700

    Refactor Remote Module
    - Change module name from Network to Remote
    - Create separate file for Constants

--- app/src/main/java/com/dicoding/bfaa/githubuser/di/RemoteModule.kt
+++ app/src/main/java/com/dicoding/bfaa/githubuser/di/RemoteModule.kt
@@ -1,20 +1,25 @@
 package com.dicoding.bfaa.githubuser.di
 
+import com.dicoding.bfaa.githubuser.BuildConfig
 import com.dicoding.bfaa.githubuser.data.network.GithubServices
 import com.dicoding.bfaa.githubuser.data.network.RemoteDataSource
+import com.dicoding.bfaa.githubuser.data.network.RemoteInterceptor
+import com.dicoding.bfaa.githubuser.utils.Constants.BASE_URL
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule {
+object RemoteModule {
     @Singleton
     @Provides
     fun provideGsonBuilder(): Gson {
@@ -24,9 +29,23 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(gson: Gson): Retrofit.Builder {
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(RemoteInterceptor())
+            .retryOnConnectionFailure(false)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+                else HttpLoggingInterceptor.Level.NONE
+            })
+            .build()
+
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson, okHttpClient: OkHttpClient): Retrofit.Builder {
         return Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
     }
 
---
+++ app/src/main/java/com/dicoding/bfaa/githubuser/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.dicoding.bfaa.githubuser.utils
+
+object Constants {
+    // Remote Constants
+    const val BASE_URL = "https://api.github.com/"
+
+}
