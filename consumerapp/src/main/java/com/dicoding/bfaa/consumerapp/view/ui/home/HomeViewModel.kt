6154bf9a5621194eafcdea1f16651861dd0908fe
Leonards03 <edwin.leonards03@gmail.com>
2021-04-23 09:36:17 +0700

    Update HomeViewModel.kt

--- consumerapp/src/main/java/com/dicoding/bfaa/consumerapp/view/ui/home/HomeViewModel.kt
+++ consumerapp/src/main/java/com/dicoding/bfaa/consumerapp/view/ui/home/HomeViewModel.kt
@@ -1,12 +1,15 @@
 package com.dicoding.bfaa.consumerapp.view.ui.home
 
 import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import com.dicoding.bfaa.consumerapp.data.model.User
 import com.dicoding.bfaa.consumerapp.data.repository.MainRepository
 import com.dicoding.bfaa.consumerapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,12 +17,20 @@ class HomeViewModel @Inject constructor(
     private val repository: MainRepository
 ) : ViewModel() {
 
-    fun getFavoriteList(context: Context) = liveData(Dispatchers.IO) {
-        emit(Resource.loading(null))
+    private val _favoriteList: MutableLiveData<Resource<List<User>>> = MutableLiveData()
+    val favoriteList: LiveData<Resource<List<User>>> get() = _favoriteList
+
+    suspend fun getFavoriteList(context: Context) = withContext(Dispatchers.IO) {
+        _favoriteList.postValue(Resource.loading(null))
         try {
-            emit(Resource.success(repository.getFavoriteList(context)))
+            _favoriteList.postValue(Resource.success(repository.getFavoriteList(context)))
         } catch (exception: Exception) {
-            emit(Resource.error(null, message = exception.message ?: "Error occured"))
+            _favoriteList.postValue(
+                Resource.error(
+                    null,
+                    message = exception.message ?: "Error occured"
+                )
+            )
         }
     }
 }
