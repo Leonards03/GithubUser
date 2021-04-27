d433ef46510e4d2f74659414aec47d054379521a
Leonards03 <edwin.leonards03@gmail.com>
2021-04-27 10:12:43 +0700

    Update variable to camelCase format

--- app/src/main/java/com/dicoding/bfaa/githubuser/data/network/RemoteInterceptor.kt
+++ app/src/main/java/com/dicoding/bfaa/githubuser/data/network/RemoteInterceptor.kt
@@ -5,11 +5,11 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class RemoteInterceptor : Interceptor {
-    private val PERSONAL_TOKEN = BuildConfig.GITHUB_PERSONAL_TOKEN
+    private val personalToken = BuildConfig.GITHUB_PERSONAL_TOKEN
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val newRequest = request.newBuilder()
-            .addHeader("Authorization", PERSONAL_TOKEN)
+            .addHeader("Authorization", personalToken)
             .build()
 
         return chain.proceed(newRequest)
